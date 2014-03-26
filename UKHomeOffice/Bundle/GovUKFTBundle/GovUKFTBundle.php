b68f985f173d9dcb78cdc9c39d873dc33e35cb99
Mike Whiting <mail@mikejw.co.uk>
2014-03-26 14:40:21 +0000

    working on bundle.

--- UKHomeOffice/Bundle/GovUKFTBundle/GovUKFTBundle.php
+++ UKHomeOffice/Bundle/GovUKFTBundle/GovUKFTBundle.php
@@ -4,55 +4,8 @@ namespace UKHomeOffice\Bundle\GovUKFTBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class GovUKFTBundle
+class GovUKFTBundle extends Bundle
 {
 
-
-
-
-    public function __construct()
-    {
-    	//
-    }
-
-
-    public function getPath()
-    {
-    	return null;
-    }
-
-
-    public function boot()
-    {
-    	//
-    }
-
-    public function build()
-    {
-    	//
-    }
-
-
-    public function setContainer()
-    {	
-    	//
-    }
-
-
-    public function getContainerExtension()
-    {
-    	return null;
-    }
-
-
-    public function getParent()
-    {
-    	return null;
-    }
-
-    public function getName()
-    {
-    	return get_class($this);
-    }
 
 }
